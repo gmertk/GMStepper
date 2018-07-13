d81e614d1b7571d005e0f5d473feb0e48f333602
Brent Whitman <brent@pathym.com>
2018-07-13 13:31:32 -0700

    Update to Swift 4.2

--- GMStepper/GMStepper.swift
+++ GMStepper/GMStepper.swift
@@ -303,7 +303,7 @@ import UIKit
 
         setupNumberFormatter()
         
-        NotificationCenter.default.addObserver(self, selector: #selector(GMStepper.reset), name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(GMStepper.reset), name: UIApplication.willResignActiveNotification, object: nil)
     }
     
     func setupNumberFormatter() {
